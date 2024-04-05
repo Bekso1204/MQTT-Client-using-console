790671ab6fa0b770a587aae9b437ae93341089c3
Alexian <alexian.pellier2912@gmail.com>
2024-04-05 19:16:03 +0200

    Ajoutez des fichiers projet.

---
+++ MQTTPublisher/Publisher.cs
@@ -0,0 +1,65 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using System;
+using System.Text.Json.Nodes;
+
+namespace MQTTPublisher
+{
+    class Publisher
+    {
+        static async Task Main(string[] args)
+        {
+            var mqttFactory = new MqttFactory();
+            var client = mqttFactory.CreateMqttClient();
+            var options = new MqttClientOptionsBuilder()
+                .WithClientId(Guid.NewGuid().ToString())
+                .WithTcpServer("test.mosquitto.org", 1883)
+                .WithCleanSession()
+                .Build();
+
+            client.UseConnectedHandler(e =>
+            {
+                Console.WriteLine("Connected to the broker successfully !");
+            });
+
+            client.UseDisconnectedHandler(e =>
+            {
+                Console.WriteLine("Disconnected from the server successfully !");
+            });
+
+            await client.ConnectAsync(options);
+
+            Console.WriteLine("Please press a key to publish the message");
+            Console.ReadLine();
+
+            await PublishMessageAsync(client);
+
+            await client.DisconnectAsync();
+        }
+
+        private static async Task PublishMessageAsync(IMqttClient client)
+        {
+            string messagePayLoad = "Hello";
+            string messagePayLoad2 = "World !";
+
+            var message = new MqttApplicationMessageBuilder()
+                .WithTopic("FirstTopic")
+                .WithPayload(messagePayLoad)
+                .WithAtLeastOnceQoS()
+                .Build();
+
+            var message2 = new MqttApplicationMessageBuilder()
+                .WithTopic("SecondTopic")
+                .WithPayload(messagePayLoad2)
+                .WithAtLeastOnceQoS()
+                .Build();
+
+            if (client.IsConnected)
+            {
+                await client.PublishAsync(message);
+                await client.PublishAsync(message2);
+            }
+        }
+    }
+}
---
+++ MQTTSubscriber/Subscriber.cs
@@ -0,0 +1,49 @@
+﻿using MQTTnet.Client.Options;
+using MQTTnet;
+using System;
+using MQTTnet.Client;
+using System.Text;
+
+namespace MQTTSubscriber
+{
+    class Subscriber
+    {
+        static async Task Main(string[] args)
+        {
+            var mqttFactory = new MqttFactory();
+            var client = mqttFactory.CreateMqttClient();
+            var options = new MqttClientOptionsBuilder()
+                .WithClientId(Guid.NewGuid().ToString())
+                .WithTcpServer("test.mosquitto.org", 1883)
+                .WithCleanSession()
+                .Build();
+
+            client.UseConnectedHandler( async e =>
+            {
+                Console.WriteLine("Connected to the broker successfully !");
+                var topicFilter = new TopicFilterBuilder()
+                .WithTopic("FirstTopic")
+                .Build();
+
+                await client.SubscribeAsync(topicFilter);
+            });
+
+            client.UseDisconnectedHandler(e =>
+            {
+                Console.WriteLine("Disconnected from the server successfully !");
+            });
+
+            client.UseApplicationMessageReceivedHandler(e =>
+            {
+                Console.WriteLine($"Received message : {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)} ");
+                Console.WriteLine($"Received topic : {(e.ApplicationMessage.Topic)}");
+            });
+
+            await client.ConnectAsync(options);
+
+            Console.ReadLine();
+
+            await client.DisconnectAsync();
+        }
+    }
+}
